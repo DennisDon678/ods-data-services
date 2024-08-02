2ea5c57e032855a5942bf19371328e9dba302d22
Dennisdon678 <dennisdon678@gmail.com>
2024-08-02 08:58:31 -0400

    tv update

--- app/Http/Controllers/TvController.php
+++ app/Http/Controllers/TvController.php
@@ -10,7 +10,12 @@ class TvController extends Controller
     //
 
     public function get_plans(Request $request){
-        $plans = Cable_plan::where('cable_id','=', $request->cable_id)->first();
+        $plans = Cable_plan::where('cable_id','=', $request->cable_id)->get();
         return response()->json($plans);
+    }
+
+    public function get_plan_info(Request $request){
+        $plan = Cable_plan::Where('plan_id','=',$request->plan_id)->first();
+        return response()->json($plan);
     }
 }
