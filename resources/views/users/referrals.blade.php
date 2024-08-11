f2abca160a01883170e3efcd92f4f22c6f7461d7
Dennisdon678 <dennisdon678@gmail.com>
2024-08-11 19:01:23 -0400

    refferal

--- resources/views/users/referrals.blade.php
+++ resources/views/users/referrals.blade.php
@@ -25,11 +25,70 @@
                                 <input type="text"
                                     value="{{ env('APP_URL') }}/auth/sign-in?referral-id={{ Auth::user()->referral_id }}"
                                     disabled="" class="form-control border-start-0" id="referral">
-                                    
+
                             </div>
                             <button class="btn btn-primary" onclick="copy()">Copy link</button>
                         </div>
 
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                @php
+                    $transactions = App\Models\User::where('referred_by', '=', Auth::user()->referral_id)
+                        ->orderby('created_at', 'ASC')
+                        ->paginate(10);
+                @endphp
+                <!-- Transactions -->
+                <div class="col-12 col-md-12 position-relative ">
+                    <div class="card border-0 overflow-hidden mb-4">
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col-9">
+                                    <div class="row">
+                                        <div class="col-auto mb-2">
+                                            <i class="bi bi-shop h5 avatar avatar-40 bg-light-theme rounded"></i>
+                                        </div>
+                                        <div class="col align-self-center mb-2">
+                                            <h6 class="fw-medium mb-0">Referral History ({{ count($transactions) }})
+                                            </h6>
+                                            <p class="text-secondary small">All Referral</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body px-4">
+                            <table class="table table-borderless">
+
+                                <tbody class="">
+                                    @forelse ($transactions as $tran)
+                                        <tr class="">
+                                            <td>
+                                                <div class="row align-items-center p-4 border rounded">
+                                                    <div class="col-12 ps-0">
+                                                        <h6 class="mb-0 bold">{{ strtoupper($tran->name) }}</h6>
+                                                        <p class="text-secondary mb-0 small">Email: <em
+                                                                class="bold">{{ $tran->email }}</em></p>
+                                                        <p class="text-secondary mb-0 small">Is Vendor? <em
+                                                                class="bold">{{ $tran->is_vendor ? 'Yes' : 'No' }}</em>
+                                                        </p>
+                                                        <p class="text-secondary mb-0 small">Referred on: <em
+                                                                class="bold">{{ $tran->created_at }}</em></p>
+
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        No referrals Yet!
+                                    @endforelse
+                                </tbody>
+                            </table>
+
+                            {{ $transactions->links('pagination::bootstrap-5') }}
+                        </div>
                     </div>
                 </div>
             </div>
