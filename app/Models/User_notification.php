b7b7b6aa5eea7075c30b79f93ff5da5f5c9b7357
DennisDon678 <dennisdon678@gmail.com>
2025-05-21 05:54:52 -0600

    notice

--- app/Models/User_notification.php
+++ app/Models/User_notification.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class User_notification extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'message',
+        'type',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
 }
